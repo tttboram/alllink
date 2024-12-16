7a31f2051a0936713c7cfd5f820db18c8ccbfb82
0704lym <0704lym@4usoft.kr>
2024-12-16 17:18:08 +0900

    설정페이지

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,20 +1,88 @@
 $(function () {
+  
   popupEvent();
+  popupClose();
   slickEvent();
+  setting() ;
+  settingCancel();
+  settingChange();
 });
 
 
 
 //팝업창 띄우기
-function popupEvent() {
-  $('#popup').css({
-    "top": (($(window).height() - $('#popup').outerHeight()) / 2 + $(window).scrollTop()) + "px",
-    "left": (($('body').width() - $('#popup').outerWidth()) / 2 + $(window).scrollLeft()) + "px",
+function popupPosition() {
+  $('.popup').css({
+    "top": (($(window).height() - $('.popup').outerHeight()) / 2 + $(window).scrollTop()) + "px",
+    "left": (($('body').width() - $('.popup').outerWidth()) / 2 + $(window).scrollLeft()) + "px",
   });
 };
+
+function popupEvent() {
+  var popupBtn = $('.popup-open a');
+  
+  popupBtn.on('click',function(){
+    popupPosition();
+    var targetPopup = $(this).data('target');
+    $('.'+targetPopup).css('display','block');
+    $('.dimmed').css('display','block');
+  })
+}
+
+function popupClose() {
+  var popupClose = $('.popup-cont .close');
+
+  popupClose.on('click', function(){
+    $('.popup').css('display','none');
+    $('.dimmed').css('display','none');
+  })
+}
 
 
 //슬릭
 function slickEvent() {
   $('.reciept-slide').slick();
+}
+
+//설정
+function setting() {
+  var settingBtn = $('.setting-area>div .btn');
+  settingBtn.on('click',function(){
+    $(this).css('display','none');
+    $(this).parent().children('.ing').css('display','flex');
+
+    if($(this).hasClass('naver')){
+      $('.state .naver').css('display','flex');
+    }else{
+      $('.state .kakao').css('display','flex');
+    }
+  })
+}
+
+//설정 해제
+function settingCancel() {
+  var cancelBtn =$('.ing .cancel');
+    cancelBtn.on('click',function(){console.log($(this));
+    $(this).parent('.ing').css('display','none');
+    $(this).parent().parent().children('.btn').css('display','flex');
+
+    if($(this).parent().hasClass('naver')){
+      $('.state .naver').css('display','none');
+    }else{
+      $('.state .kakao').css('display','none');
+    }
+  })
+}
+
+//변경버튼 
+function settingChange() {
+  var changeBtn = $('.default .change');
+  changeBtn.on('click',function(){
+    $('.setting-area').toggleClass('on');
+    if($('.setting-area').hasClass('on')){
+      $(this).text('취소');
+    }else {
+      $(this).text('변경');
+    }
+  })
 }
