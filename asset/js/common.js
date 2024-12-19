44bce100461e62a8f51ec01ec7d830b48f64f9d1
0704lym <0704lym@4usoft.kr>
2024-12-19 13:54:48 +0900

    모바일 크롬, 사파리 확인

--- asset/js/common.js
+++ asset/js/common.js
@@ -6,6 +6,7 @@ $(function () {
   setting() ;
   settingCancel();
   settingChange();
+  detectBrowser();
 });
 
 
@@ -99,4 +100,19 @@ function setFullHeight() {
 
 window.addEventListener('resize', setFullHeight);
 setFullHeight();
+
+function detectBrowser() {
+  const userAgent = navigator.userAgent;
+
+  // 안드로이드 크롬
+  if (/Android/i.test(userAgent) && /Chrome/i.test(userAgent)) {
+    $('.main').css('height','calc(var(--vh, 1vh)*100 - 76px');
+  }
+
+  // 아이폰 사파리
+  else if (/iPhone/i.test(userAgent) && /Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)) {
+    ('.main').css('height','calc(var(--vh, 1vh)*100 - 76px');
+  }
+
+}
 
